6012074b5b150136a394119312710abb90d9b07e
aeno <s.rieke@gmail.com>
2019-06-03 15:23:22 +0200

    Inject button on every backbone route change

--- harvest_newproject.user.js
+++ harvest_newproject.user.js
@@ -4,37 +4,43 @@
 // @homepage     https://github.com/aeno/harvestapp-new-project-btn
 // @updateURL    https://github.com/aeno/harvestapp-new-project-btn/raw/master/harvest_newproject.user.js
 // @supportURL   https://github.com/aeno/harvestapp-new-project-btn/issues
-// @version      0.2
+// @version      0.3
 // @description  Adds a small "New Project" button to your timesheet, right under the "New Entry" button
 // @author       Aeno<tampermonkey@aenogym.de>
 // @match        https://*.harvestapp.com/time
 // @match        https://*.harvestapp.com/time/
 // @match        https://*.harvestapp.com/time/day/*
 // @grant        none
+// @run-at document-idle
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var container = document.body.querySelector(".new-time-entry-container");
-    var button = document.createElement("a");
-    container.appendChild(button)
-    button.outerHTML =
-        "<a class=\"button-new-time-entry\" style=\""+
-        "    top: 100px;"+
-        "    font-size: 11px;"+
-        "    text-decoration: none;"+
-        "\" href=\"/projects/new\">"+
-        "    <span class=\"hui-button w-100 p-0\" style=\""+
-        "    font-size: 25px;"+
-        "    line-height: 35px;"+
-        "\">"+
-        "      P"+
-        "    </span>"+
-        "    <span class=\"mt-5\" style=\""+
-        "    display: block;"+
-        "\">"+
-        "      New&nbsp;Project"+
-        "    </span>"+
-        "  </a>";
+    function injectButton() {
+        var container = document.body.querySelector(".new-time-entry-container");
+        var button = document.createElement("a");
+        container.appendChild(button)
+        button.outerHTML =
+            "<a class=\"button-new-time-entry\" style=\""+
+            "    top: 100px;"+
+            "    font-size: 11px;"+
+            "    text-decoration: none;"+
+            "\" href=\"/projects/new\">"+
+            "    <span class=\"hui-button w-100 p-0\" style=\""+
+            "    font-size: 25px;"+
+            "    line-height: 35px;"+
+            "\">"+
+            "      P"+
+            "    </span>"+
+            "    <span class=\"mt-5\" style=\""+
+            "    display: block;"+
+            "\">"+
+            "      New&nbsp;Project"+
+            "    </span>"+
+            "  </a>";
+    }
+
+    Backbone.history.on('route', injectButton);
+    injectButton();
 })();
